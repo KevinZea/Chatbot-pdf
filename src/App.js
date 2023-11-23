5e02e2c73d3ef57e1c03c2b04b6707738f02040b
Kevin <kevinstevenzeasuarez@gmail.com>
2023-11-23 13:12:15 -0500

    change design for aasesor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import { createChat } from './chat';
 import LinkRenderer from './LinkRenderer/LinkRenderer';
 import AddToHomeScreenButton from './AddToHomeScreenButton/AddToHomeScreenButton';
-import logo from './resources/logo.png'
+import icono from './resources/logo.png'
+
 
 function App() {
   const [chats, setChats] = useState([])
   const [prompt, setPrompt] = useState('')
+  const chatContainerRef = useRef(null);
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+  const [imageFixed, setImageFixed] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    // Retirar el controlador de eventos al desmontar el componente
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   function promptHandle(e) {
     setPrompt(e.target.value)
@@ -26,7 +56,6 @@ function App() {
       chats.pop()
       setChats([...chats])
       stateChats(response)
-
       // console.log(chats)
     }
   }
@@ -40,43 +69,63 @@ function App() {
       sendMessages(e)
     }
   }
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      scrollToBottom();
+    }
+  }, [chats]);
+  const scrollToBottom = () => {
+    chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+  };
+
+  const handleScroll = () => {
+    // Verificar la posición de desplazamiento y aplicar la clase CSS según sea necesario
+    if (window.scrollY > 10) {
+      setImageFixed(true);
+    } else {
+      setImageFixed(false);
+    }
+  };
 
 
+  const smallScreen = windowSize.width < 768
+  const enlace = "https://citybot.info/"
   return (
     <div className='app'>
       <nav className='navbar'>
         <h1>Citybot</h1>
-        <img src={logo} width={30} height={30}></img>
+        <div className='navbar-menu'>
+          <a href={enlace} target='_blank'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
+              <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z" />
+            </svg>
+          </a>
+        </div>
       </nav>
       <div className='chat'>
         {chats.length < 1 ? (
           <div className='chat-baner'>
-            {/* <div className='chat-baner-icon'>
-              <img src={logo} width={30} height={30}></img>
 
-            </div> */}
             <div className='chat-baner-info'>
               <span>
                 ¡Bienvenido a CityBot!
               </span>
               <p>
                 Tu asistente virtual de viajes.
-                <br></br>
                 Aquí encontrarás recomendaciones sobre hotelería, gastronomía y servicios turísticos del eje cafetero.
-                <br></br>
-                También puedes ingresar a nuestra guia web <a href='https://citybot.info/' target='_blank'>citybot</a>
+                También puedes ingresar a nuestra guia web citybot
                 <br></br>
                 <br></br>
                 Para hacer buen uso del chatbot, recuerda hacer preguntas de manera exacta e indicando siempre el destino.
-                <br></br>
                 Puedes iniciar una conversación o probar los siguientes ejemplos:
               </p>
               <div className='chat-baner-info-buttons'>
-                <button onClick={(e) => { setPrompt("5 cosas para hacer en Filandia") }}>
-                  5 cosas para hacer en Filandia
+
+                <button onClick={(e) => { setPrompt("5 Cosas para hacer en Filandia") }}>
+                  5 Cosas para hacer en Filandia
                 </button>
                 <button onClick={(e) => { setPrompt("¿Que comer en Santa Rosa de Cabal?") }}>
-                  ¿Que comer en Santa Rosa de Cabal? 
+                  ¿Que comer en Santa Rosa de Cabal?
                 </button>
                 <button onClick={(e) => { setPrompt("¿Como llegar al Valle del Cocora?") }}>
                   ¿Como llegar al Valle del Cocora?
@@ -84,18 +133,39 @@ function App() {
               </div>
 
             </div>
+
           </div>
 
         ) :
 
-          <div className='chat-messages'>
+          <div className='chat-messages' ref={chatContainerRef}>
+            {smallScreen && (
+              <div></div>
+              // <div className={imageFixed ? 'slider' : 'carrusel'}>
+              //   <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+              //     <div class="carousel-inner">
+              //       <div class="carousel-item active">
+              //         <img src={banner} class="d-block w-100" alt="..." />
+              //       </div>
+              //       <div class="carousel-item">
+              //         <img src={bannerDos} class="d-block w-100" alt="..." />
+              //       </div>
+              //       <div class="carousel-item">
+              //         <img src={bannerTres} class="d-block w-100" alt="..." />
+              //       </div>
+              //     </div>
+              //   </div>
+              // </div>
+            )}
             {
               chats.length >= 1 && (
                 chats.map((c) => {
                   return (
                     c.role === "assistant" ? (
                       <div className='chat-asistent'>
-                        <img src={logo} width={20} height={20}></img>
+                        <div className='chat-asistent-icon'>
+                          <img src={icono}></img>
+                        </div>
                         <span><LinkRenderer text={c.content} /></span>
                       </div>
                     ) :
